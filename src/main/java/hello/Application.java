fdac415f1b4cedd5600874adf5f3998f3ddd3c45
Dave Syer <dsyer@pivotal.io>
2018-08-16 12:59:57 +0100

    Update for LAZY mode (new feature)

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,8 +2,11 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.config.BootstrapMode;
 
 @SpringBootApplication
+@EnableJpaRepositories(bootstrapMode=BootstrapMode.LAZY)
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class);
